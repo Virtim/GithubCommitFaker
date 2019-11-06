784bb4fdd53ea1165891c1f4dbaa45826e20ef07
Timur Minnakhmetov <timvir@gmail.com>
2019-11-06 15:39:01 -0800

    Add CI

--- run.py
+++ run.py
@@ -4,35 +4,22 @@ import fileinput, sys
 import os.path
 import random
 
-statusCommand = 'git status'
-addCommand = "git add ."
-commitCommand = 'git commit -m "Added"'
+commitCommand = 'git commit --allow-empty -m commit'
 pushCommand = "git push"
 removeIndexLock = "sudo rm -rf ./.git/index.lock"
-commitCounter = 0
+timeToSleep = 2
+commitCounter = 25
 
-while commitCounter > -1:
-    try:
-        os.remove('./.git/index.lock')
-    except OSError:
-        print os.path.isfile('./.git/index.lock')
-    timeToSleep = random.randint(1,43200)
-    print "Going to sleep for ", timeToSleep, " seconds"
-    for line in fileinput.input(["test.txt"], inplace=True):
-        if "car" in line:
-            line = line.replace("car", "truck")
-            # sys.stdout is redirected to the file
-            sys.stdout.write(line)
-        elif "truck" in line:
-            line = line.replace("truck", "car")
-            # sys.stdout is redirected to the file
-            sys.stdout.write(line)
-    process = subprocess.Popen(statusCommand.split(), stdout=subprocess.PIPE)
-    process = subprocess.Popen(addCommand.split(), stdout=subprocess.PIPE)
+while commitCounter > 0:
+    # try:
+    #     os.remove('./.git/index.lock')
+    # except OSError:
+    #     print "Index lock doesnt exist"
+    # print "Going to sleep for ", timeToSleep, " seconds"
     process = subprocess.Popen(commitCommand.split(), stdout=subprocess.PIPE)
     time.sleep(1)
     process = subprocess.Popen(pushCommand.split(), stdout=subprocess.PIPE)
     time.sleep(1)
-    print "One more commit added to github! It's ", commitCounter, " since we started"
-    commitCounter += 1
+    print("One more commit added to github! Only ", commitCounter, " left until script is done")
+    commitCounter -= 1
     time.sleep(timeToSleep)
