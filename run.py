f572a03c8033727167460068bf590ebc6d982eae
Timur Minnakhmetov <timvir@gmail.com>
2019-11-06 16:47:55 -0800

    meh

--- run.py
+++ run.py
@@ -6,10 +6,11 @@ import random
 
 commitMessage = os.environ['CIRCLE_SHA1']
 commitCommand = "git commit --allow-empty -m {}".format(commitMessage)
-pushCommand = "git push origin master -f"
+mergeMasterIn = "git merge origin master --no-edit"
+pushCommand = "git push -f"
 removeIndexLock = "sudo rm -rf ./.git/index.lock"
 timeToSleep = 2
-commitCounter = 25
+commitCounter = 5
 
 while commitCounter > 0:
     # try:
@@ -17,10 +18,11 @@ while commitCounter > 0:
     # except OSError:
     #     print "Index lock doesnt exist"
     # print "Going to sleep for ", timeToSleep, " seconds"
+    # process = subprocess.Popen(mergeMasterIn.split(), stdout=subprocess.PIPE)
     process = subprocess.Popen(commitCommand.split(), stdout=subprocess.PIPE)
     time.sleep(1)
     process = subprocess.Popen(pushCommand.split(), stdout=subprocess.PIPE)
     time.sleep(1)
-    print("One more commit added to github! Only {commitCounter} left until script is done")
+    print("One more commit added to github! Only {} commits left until script is done".format(commitCounter))
     commitCounter -= 1
     time.sleep(timeToSleep)
